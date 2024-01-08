6dc5d0e6466ad73a79b5bdf2beec2ab6195f35b1
matias acosta <acosta_matias8@hotmail.com>
2024-01-08 16:42:17 -0300

    animation album

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade': ' fadeIn 3s ease-in-out;'
+      },
+      keyframes: {
+        'fadeIn': {
+          '0%': {
+            opacity: '0'
+          },
+          '100%': {
+            opacity: '1'
+          }
+        }
+
+      }
+    },
   },
   plugins: [],
 }
