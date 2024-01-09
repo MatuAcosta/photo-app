eac870c363d78ab536b942e586ef3c51d0036914
Matias Emanuel Acosta <83653785+MatuAcosta@users.noreply.github.com>
2024-01-09 18:39:52 -0300

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = { 
-    "projectId": "photo-app-cbdeb", 
-    "appId": "1:563996376380:web:64782452c5e52634d87239",
-    "storageBucket": "photo-app-cbdeb.appspot.com", 
-    "apiKey": "AIzaSyALy3ifVcCWtUbAqAS766EqjIu7D7qEBHk", 
-    "authDomain": "photo-app-cbdeb.firebaseapp.com", 
-    "messagingSenderId": "563996376380", 
-    "measurementId": "G-DFX574X7K9" 
-};
