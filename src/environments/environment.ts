50622aa3dd8430defa80f610f3a342fb37dcd642
matias acosta <acosta_matias8@hotmail.com>
2024-01-09 20:27:42 -0300

    DEPLOY WILL WORK

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = { 
+    "projectId": import.meta.env['NG_APP_projectId'], 
+    "appId": import.meta.env['NG_APP_appId'],
+    "storageBucket": import.meta.env['NG_APP_storageBucket'], 
+    "apiKey": import.meta.env['NG_APP_apiKey'], 
+    "authDomain":  import.meta.env['NG_APP_authDomain'], 
+    "messagingSenderId": import.meta.env['NG_APP_messagingSenderId'], 
+    "measurementId": import.meta.env['NG_APP_measurementId'] 
+};
